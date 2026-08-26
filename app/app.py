1b9b4e15af2194c9fb7b2f7cac64b13316cac6fa
Mélinda <meldu731@gmail.com>
2026-08-26 13:39:58 +0200

    Ajout de l'application Python

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Bonjour depuis mon projet DevOps !")
+
+server = HTTPServer(("0.0.0.0", 8080), Handler)
+
+print("Application demarree sur le port 8080")
+
+server.serve_forever()
